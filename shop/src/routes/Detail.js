a041c18d9ffa802e5255d6206ee53e941dc78ce8
Indu <wjsdlsdn1158@naver.com>
2024-08-12 18:27:13 +0900

    [feat] styled components

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Detail(props) {
 
   let { id } = useParams();
-  let 찾은상품 = props.shoes.find(function(x){
+  let 찾은상품 = props.shoes.find(function (x) {
     return x.id == id
   });
 
@@ -11,17 +12,17 @@ function Detail(props) {
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img src={`https://codingapple1.github.io/shop/shoes${찾은상품.id+1}.jpg`} width="100%" />
+          <img src={`https://codingapple1.github.io/shop/shoes${찾은상품.id + 1}.jpg`} width="100%" />
         </div>
         <div className="col-md-6 mt-4">
           <h4 className="pt-5">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}원</p>
-          <button className="btn btn-danger">주문하기</button> 
+          <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
-  </div>  
+    </div>
   )
-  }
+}
 
-  export default Detail;
+export default Detail;
