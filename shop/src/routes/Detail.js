26df378fec26e3e11fa721ed91a1b48670f94f43
Indu <wjsdlsdn1158@naver.com>
2024-08-26 15:43:19 +0900

    [feat] Detail UI 변경

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import {Nav} from "react-bootstrap"
 
 function Detail(props) {
 
@@ -11,6 +12,7 @@ function Detail(props) {
 
   let [alert_Hidden, setAlertHidden] = useState(false);
   let [user_input, isInt] = useState('');
+  let [tab, changeTab] = useState(0);
 
   useEffect(() => {
     let a = setTimeout(() => { setAlertHidden(true) }, 2000)
@@ -27,7 +29,6 @@ function Detail(props) {
       {alert_Hidden ? null : (<div className="alert alert-warning">
         2초 이내 구매시 할인
       </div>)}
-      <input className="onlyInt" onChange={(e) => isInt(e.target.value)} value={user_input} />
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${찾은상품.id + 1}.jpg`} width="100%" />
@@ -39,8 +40,34 @@ function Detail(props) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="link0" onClick={() => {changeTab(0)}}>버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link1" onClick={() => {changeTab(1)}}>버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2" onClick={() => {changeTab(2)}}>버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <TabContent tab={tab}/>
     </div>
   )
+}
+
+function TabContent(props){
+  if(props.tab == 0){
+    return <div>내용1</div>
+  }
+  else if(props.tab==1){
+    return <div>내용2</div>
+  }
+  else{
+    return <div>내용3</div>
+  }
 }
 
 export default Detail;
